ae67dcea4892651d312f0e29cc697f1cc4b18e19
Mitch Capper <mitch.capper@gmail.com>
2023-03-28 16:40:39 -0700

    ConPTY Resuse/re-parenting support, win32-input-mode and higher performance output handling
    Fixed win32-input-mode to work.
    Prevent ConPTY running in design mode.
    Made disconnecting the ConPTY easier from the control (if to reparent).
    Read bigger junks in the output loop, it still returns instantly so should only be perf gains.
    Prevent ConPTY from having certain things run more than once (Ie when attached to a second terminal)

--- ConPtyTermEmulatorLib/Term.cs
+++ ConPtyTermEmulatorLib/Term.cs
@@ -1,13 +1,12 @@
 using Microsoft.Terminal.Wpf;
 using Microsoft.Win32.SafeHandles;
 using System;
-using System.Diagnostics;
 using System.IO;
 using Windows.Win32;
 using System.Threading.Tasks;
-using System.Threading;
 using System.Text;
 using System.Text.RegularExpressions;
+
 
 namespace ConPtyTermEmulatorLib {
 	/// <summary>
@@ -18,9 +17,9 @@ namespace ConPtyTermEmulatorLib {
 		private SafeFileHandle _consoleInputPipeWriteHandle;
 		private StreamWriter _consoleInputWriter;
 		public StringBuilder ConsoleOutputLog { get; private set; }
-		private static Regex NewlineReduce = new (@"\n\s*?\n\s*?[\s]+", RegexOptions.Singleline);
+		private static Regex NewlineReduce = new(@"\n\s*?\n\s*?[\s]+", RegexOptions.Singleline);
 		public static Regex colorStrip = new(@"((\x1B\[\??[0-9;]*[a-zA-Z])|\uFEFF|\u200B|\x1B\]0;|[\a\b])", RegexOptions.Compiled | RegexOptions.ExplicitCapture); //also strips BOM, bells, backspaces etc
-		public string GetConsoleText(bool stripVTCodes=true) => NewlineReduce.Replace( (stripVTCodes ? StripColors(ConsoleOutputLog.ToString()) : ConsoleOutputLog.ToString()).Replace("\r",""),"\n\n").Trim();
+		public string GetConsoleText(bool stripVTCodes = true) => NewlineReduce.Replace((stripVTCodes ? StripColors(ConsoleOutputLog.ToString()) : ConsoleOutputLog.ToString()).Replace("\r", ""), "\n\n").Trim();
 
 		public static string StripColors(String str) {
 			return colorStrip.Replace(str, "");
@@ -36,6 +35,7 @@ namespace ConPtyTermEmulatorLib {
 		/// </summary>
 		public event EventHandler TermReady;
 		public event EventHandler<TerminalOutputEventArgs> TerminalOutput;//how we send data to the UI terminal
+		public bool TermProcIsStarted { get; private set; }
 
 
 		/// <summary>
@@ -45,19 +45,25 @@ namespace ConPtyTermEmulatorLib {
 		/// <param name="command">the command to run, e.g. cmd.exe</param>
 		/// <param name="consoleHeight">The height (in characters) to start the pseudoconsole with. Defaults to 80.</param>
 		/// <param name="consoleWidth">The width (in characters) to start the pseudoconsole with. Defaults to 30.</param>
-		public void Start(string command, int consoleWidth = 80, int consoleHeight = 30, bool logOutput=false) {
+		public void Start(string command, int consoleWidth = 80, int consoleHeight = 30, bool logOutput = false) {
+			if (Process != null)
+				throw new Exception("Called Start on ConPTY term after already started");
 			if (IsDesignMode) {
+				TermProcIsStarted = true;
 				TermReady?.Invoke(this, EventArgs.Empty);
+				return;
 			}
 			if (logOutput)
 				ConsoleOutputLog = new();
 			using (var inputPipe = new PseudoConsolePipe())
 			using (var outputPipe = new PseudoConsolePipe())
 			using (var pseudoConsole = PseudoConsole.Create(inputPipe.ReadSide, outputPipe.WriteSide, consoleWidth, consoleHeight))
-			using (var process = ProcessFactory.Start(command, PInvoke.PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE, pseudoConsole.Handle)) {
+			using (var process = ProcessFactory.Start(command, PInvoke.PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE, pseudoConsole)) {
+				Process = process;
 				TheConsole = pseudoConsole;
 				// copy all pseudoconsole output to a FileStream and expose it to the rest of the app
 				ConsoleOutStream = new FileStream(outputPipe.ReadSide, FileAccess.Read);
+				TermProcIsStarted = true;
 				TermReady?.Invoke(this, EventArgs.Empty);
 				// Store input pipe handle, and a writer for later reuse
 				_consoleInputPipeWriteHandle = inputPipe.WriteSide;
@@ -68,11 +74,12 @@ namespace ConPtyTermEmulatorLib {
 				// free resources in case the console is ungracefully closed (e.g. by the 'x' in the window titlebar)
 				OnClose(() => DisposeResources(process, pseudoConsole, outputPipe, inputPipe, _consoleInputWriter));
 
-				WaitForExit(process).WaitOne(Timeout.Infinite);
-				TerminalOutput?.Invoke(this, new TerminalOutputEventArgs("Session Terminated"));
+				process.Process.WaitForExit();
+				WriteToUITerminal("Session Terminated");
 				TheConsole.Dispose();
 			}
 		}
+		public ProcessFactory.WrappedProcess Process;
 		PseudoConsole TheConsole;
 		/// <summary>
 		/// Sends the given string to the anonymous pipe that writes to the active pseudoconsole.
@@ -83,21 +90,15 @@ namespace ConPtyTermEmulatorLib {
 				return;
 			if (TheConsole.IsDisposed)
 				return;
-			if (_consoleInputWriter == null) {
+			if (_consoleInputWriter == null)
 				throw new InvalidOperationException("There is no writer attached to a pseudoconsole. Have you called Start on this instance yet?");
-			}
-			Debug.WriteLine("writing to terminal: " + input);
+
 			_consoleInputWriter.Write(input);
 		}
-
-		/// <summary>
-		/// Get an AutoResetEvent that signals when the process exits
-		/// </summary>
-		private static AutoResetEvent WaitForExit(Process process) =>
-			new AutoResetEvent(false) {
-				SafeWaitHandle = new SafeWaitHandle(process.ProcessInfo.hProcess, ownsHandle: false)
-			};
-
+		public void StopExternalTermOnly() {
+			if (Process?.Process?.HasExited != false) return;
+			Process.Process?.Kill();
+		}
 		/// <summary>
 		/// Set a callback for when the terminal is closed (e.g. via the "X" window decoration button).
 		/// Intended for resource cleanup logic.
@@ -120,33 +121,38 @@ namespace ConPtyTermEmulatorLib {
 
 		public void Start() {
 			if (IsDesignMode) {
-				TerminalOutput?.Invoke(this,new ("MyShell DesignMode:> Your command window content here\r\n"));
+				WriteToUITerminal("MyShell DesignMode:> Your command window content here\r\n");
 				return;
 			}
-			Task.Run(() => ReadOutputLoop());
+			Task.Run(ReadOutputLoop);
 		}
-		const int READ_BUFFER_SIZE = 1;
-		public void WinDirectInputMode(bool enable) {
+		private void WriteToUITerminal(in string str) {
+			TerminalOutput?.Invoke(this, new TerminalOutputEventArgs(str));
+		}
+		public bool ReadLoopStarted = false;
 
+		/// <summary>
+		/// Sets if the GUI Terminal control communicates to ConPTY using extended key events (handles certain control sequences better)
+		/// https://github.com/microsoft/terminal/blob/main/doc/specs/%234999%20-%20Improved%20keyboard%20handling%20in%20Conpty.md
+		/// </summary>
+		/// <param name="enable"></param>
+		public void Win32DirectInputMode(bool enable) {
 			var decSet = enable ? "h" : "l";
 			var str = $"\x1b[?9001{decSet}";
-			TerminalOutput?.Invoke(this, new TerminalOutputEventArgs(str));
-			//var cur = TheConsole.GetInputMode();
-			//if (enable)
-			//    cur |= CONSOLE_MODE.ENABLE_VIRTUAL_TERMINAL_INPUT;
-			//else
-			//    cur &= ~CONSOLE_MODE.ENABLE_VIRTUAL_TERMINAL_INPUT;
-			//TheConsole.SetInputMode(cur);
-			WriteToTerm(str);
+			WriteToUITerminal(str);
 		}
+		const int READ_BUFFER_SIZE = 1024;
 		private void ReadOutputLoop() {
+			if (ReadLoopStarted)
+				return;
+			ReadLoopStarted = true;
 			using (StreamReader reader = new StreamReader(ConsoleOutStream)) {
-				// Read the console's output 1 character at a time
 				int bytesRead;
 				var buf = new char[READ_BUFFER_SIZE];
+				
 				while ((bytesRead = reader.Read(buf, 0, READ_BUFFER_SIZE)) != 0) {
 					var str = new string(buf, 0, bytesRead);
-					TerminalOutput?.Invoke(this, new TerminalOutputEventArgs(str));
+					WriteToUITerminal(str);
 					if (ConsoleOutputLog != null)
 						ConsoleOutputLog.Append(str);
 
@@ -157,10 +163,12 @@ namespace ConPtyTermEmulatorLib {
 
 		void ITerminalConnection.WriteInput(string data) => WriteToTerm(data);
 
-		public void Resize(uint rows, uint columns) {
-			TheConsole?.Resize((int)columns, (int)rows);
+		void ITerminalConnection.Resize(uint row_height, uint column_width) {
+			TheConsole?.Resize((int)column_width, (int)row_height);
 		}
-
+		public void Resize(int column_width, int row_height) {
+			TheConsole?.Resize(column_width, row_height);
+		}
 		void ITerminalConnection.Close() {
 			TheConsole?.Dispose();
 		}
