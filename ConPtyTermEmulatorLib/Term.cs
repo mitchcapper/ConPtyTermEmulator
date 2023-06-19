5d602a3121316f981586536b59fab36135c9f493
Mitch Capper <mitch.capper@gmail.com>
2023-06-19 16:22:00 -0700

    Added buffer controls, ability to use binary writer, and some basic readonly/cursor visbility control

--- ConPtyTermEmulatorLib/Term.cs
+++ ConPtyTermEmulatorLib/Term.cs
@@ -6,16 +6,25 @@ using Windows.Win32;
 using System.Threading.Tasks;
 using System.Text;
 using System.Text.RegularExpressions;
-
+using System.Diagnostics;
+using System.Threading;
 
 namespace ConPtyTermEmulatorLib {
 	/// <summary>
 	/// Class for managing communication with the underlying console, and communicating with its pseudoconsole.
 	/// </summary>
 	public class Term : ITerminalConnection {
+
 		private static bool IsDesignMode = System.ComponentModel.DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject());
 		private SafeFileHandle _consoleInputPipeWriteHandle;
 		private StreamWriter _consoleInputWriter;
+		private BinaryWriter _consoleInputWriterB;
+		public Term(int READ_BUFFER_SIZE = 1024 * 16, bool USE_BINARY_WRITER = false) {
+			this.READ_BUFFER_SIZE = READ_BUFFER_SIZE;
+			this.USE_BINARY_WRITER = USE_BINARY_WRITER;
+		}
+		private bool USE_BINARY_WRITER;
+
 		public StringBuilder ConsoleOutputLog { get; private set; }
 		private static Regex NewlineReduce = new(@"\n\s*?\n\s*?[\s]+", RegexOptions.Singleline);
 		public static Regex colorStrip = new(@"((\x1B\[\??[0-9;]*[a-zA-Z])|\uFEFF|\u200B|\x1B\]0;|[\a\b])", RegexOptions.Compiled | RegexOptions.ExplicitCapture); //also strips BOM, bells, backspaces etc
@@ -64,18 +73,23 @@ namespace ConPtyTermEmulatorLib {
 				// copy all pseudoconsole output to a FileStream and expose it to the rest of the app
 				ConsoleOutStream = new FileStream(outputPipe.ReadSide, FileAccess.Read);
 				TermProcIsStarted = true;
+
 				TermReady?.Invoke(this, EventArgs.Empty);
+
 				// Store input pipe handle, and a writer for later reuse
 				_consoleInputPipeWriteHandle = inputPipe.WriteSide;
-				_consoleInputWriter = new StreamWriter(new FileStream(_consoleInputPipeWriteHandle, FileAccess.Write)) {
-					AutoFlush = true
-				};
-
+				var st = new FileStream(_consoleInputPipeWriteHandle, FileAccess.Write);
+				if (!USE_BINARY_WRITER)
+					_consoleInputWriter = new StreamWriter(st) { AutoFlush = true };
+				else
+					_consoleInputWriterB = new BinaryWriter(st);
 				// free resources in case the console is ungracefully closed (e.g. by the 'x' in the window titlebar)
+				ReadOutputLoop();
 				OnClose(() => DisposeResources(process, pseudoConsole, outputPipe, inputPipe, _consoleInputWriter));
 
 				process.Process.WaitForExit();
 				WriteToUITerminal("Session Terminated");
+
 				TheConsole.Dispose();
 			}
 		}
@@ -85,15 +99,37 @@ namespace ConPtyTermEmulatorLib {
 		/// Sends the given string to the anonymous pipe that writes to the active pseudoconsole.
 		/// </summary>
 		/// <param name="input">A string of characters to write to the console. Supports VT-100 codes.</param>
-		public void WriteToTerm(string input) {
+		public void WriteToTerm(ReadOnlySpan<char> input) {
 			if (IsDesignMode)
 				return;
 			if (TheConsole.IsDisposed)
 				return;
-			if (_consoleInputWriter == null)
+			if (_consoleInputWriter == null && _consoleInputWriterB == null)
 				throw new InvalidOperationException("There is no writer attached to a pseudoconsole. Have you called Start on this instance yet?");
-
-			_consoleInputWriter.Write(input);
+			//Debug.WriteLine($"Term.cs WriteToTerm writing: {input.ToString().Replace("\n", "\n\t").Trim()}");
+			if (!USE_BINARY_WRITER)
+				_consoleInputWriter.Write(input);
+			else
+				WriteToTermBinary(Encoding.UTF8.GetBytes(input.ToString()));
+		}
+		public void WriteToTermBinary(ReadOnlySpan<byte> input) {
+			if (!USE_BINARY_WRITER) {
+				WriteToTerm(Encoding.UTF8.GetString(input));
+				return;
+			}
+			_consoleInputWriterB.Write(input);
+			_consoleInputWriterB.Flush();
+		}
+		/// <summary>
+		/// Close the input stream to the process (will send EOF if attempted to be read). 
+		/// </summary>
+		public void CloseStdinToApp() {
+			_consoleInputWriter?.Close();
+			_consoleInputWriter?.Dispose();
+			_consoleInputWriterB?.Close();
+			_consoleInputWriterB?.Dispose();
+			_consoleInputWriter = null;
+			_consoleInputWriterB = null;
 		}
 		public void StopExternalTermOnly() {
 			if (Process?.Process?.HasExited != false) return;
@@ -104,7 +140,6 @@ namespace ConPtyTermEmulatorLib {
 		/// Intended for resource cleanup logic.
 		/// </summary>
 		private static void OnClose(Action handler) {
-
 			PInvoke.SetConsoleCtrlHandler(eventType => {
 				if (eventType == PInvoke.CTRL_CLOSE_EVENT) {
 					handler();
@@ -124,10 +159,25 @@ namespace ConPtyTermEmulatorLib {
 				WriteToUITerminal("MyShell DesignMode:> Your command window content here\r\n");
 				return;
 			}
+
 			Task.Run(ReadOutputLoop);
 		}
-		private void WriteToUITerminal(in string str) {
-			TerminalOutput?.Invoke(this, new TerminalOutputEventArgs(str));
+
+		/// <summary>
+		/// Note if you change the span to a 0 length then no input will be passed on
+		/// </summary>
+		/// <param name="str"></param>
+		public delegate void InterceptDelegate(ref Span<char> str);
+
+		public InterceptDelegate InterceptOutputToUITerminal;
+		public InterceptDelegate InterceptInputToTermApp;
+		/// <summary>
+		/// This simulates output from the program itself to the terminal window, ANSI sequences can be sent here as well
+		/// </summary>
+		/// <param name="str"></param>
+		public void WriteToUITerminal(ReadOnlySpan<char> str) {
+			//Debug.WriteLine($"Term.cs WriteToUITerminal got: {str.ToString().Replace("\n","\n\t").Trim()}");
+			TerminalOutput?.Invoke(this, new TerminalOutputEventArgs(str.ToString()));
 		}
 		public bool ReadLoopStarted = false;
 
@@ -141,27 +191,67 @@ namespace ConPtyTermEmulatorLib {
 			var str = $"\x1b[?9001{decSet}";
 			WriteToUITerminal(str);
 		}
-		const int READ_BUFFER_SIZE = 1024;
-		private void ReadOutputLoop() {
+		int READ_BUFFER_SIZE;
+		protected ref struct ReadState {
+			public Span<char> entireBuffer;
+			public Span<char> curBuffer;
+			public int readChars;
+		}
+		protected virtual void ReadOutputLoop() {
 			if (ReadLoopStarted)
 				return;
 			ReadLoopStarted = true;
+			// We have a few ways to handle the buffer with a delimiter but given the size of the buffer and the fairly cheap cost of copying, the ability to let the span be modified before passing it on, we will just copy any parts before the next delimiter to the start of the buffer when reaching the end.
 			using (StreamReader reader = new StreamReader(ConsoleOutStream)) {
-				int bytesRead;
-				var buf = new char[READ_BUFFER_SIZE];
-				
-				while ((bytesRead = reader.Read(buf, 0, READ_BUFFER_SIZE)) != 0) {
-					var str = new string(buf, 0, bytesRead);
-					WriteToUITerminal(str);
-					if (ConsoleOutputLog != null)
-						ConsoleOutputLog.Append(str);
+				ReadState state = new() { entireBuffer=new char[READ_BUFFER_SIZE]};
 
+				state.curBuffer = state.entireBuffer.Slice(0);
+				var empty = Span<char>.Empty;
 
+				while ((state.readChars = reader.Read(state.curBuffer)) != 0) {
+					//					Debug.WriteLine($"Read: {read}");
+
+					var sendSpan = HandleRead(ref state);
+
+					if (sendSpan != empty) {
+						InterceptOutputToUITerminal?.Invoke(ref sendSpan);
+						if (sendSpan.Length > 0) {
+							var str = sendSpan.ToString();
+							WriteToUITerminal(str);
+							ConsoleOutputLog?.Append(str);
+						}
+					}
 				}
 			}
 		}
+		/// <summary>
+		/// return the span (if any) to send to client, and the curBuffer
+		/// </summary>
+		/// <param name="curBuffer"></param>
+		/// <returns></returns>
+		protected virtual Span<char> HandleRead(ref ReadState state) {
+			return state.curBuffer.Slice(0, state.readChars);
+		}
 
-		void ITerminalConnection.WriteInput(string data) => WriteToTerm(data);
+
+		/// <summary>
+		/// When set to true and input from the UI WPF Terminal Control will be ignored, will still invoke the intercept event.  You can still call WriteToTerm to write to the app yourself.
+		/// </summary>
+		/// <param name="readOnly">Enable / Disable readonly mode</param>
+		/// <param name="updateCursor">Will hide/show the cursor depending on readonly setting</param>
+		public void SetReadOnly(bool readOnly = true, bool updateCursor = true) {
+			_ReadOnly = readOnly;
+			if (updateCursor)
+				SetCursorVisibility(!readOnly);
+		}
+		protected bool _ReadOnly;
+
+		void ITerminalConnection.WriteInput(string data) {
+			Span<char> span = data.ToCharArray();
+			InterceptInputToTermApp?.Invoke(ref span);
+			if (span.Length > 0 && !_ReadOnly)
+				WriteToTerm(span);
+		}
 
 		void ITerminalConnection.Resize(uint row_height, uint column_width) {
 			TheConsole?.Resize((int)column_width, (int)row_height);
@@ -169,6 +259,13 @@ namespace ConPtyTermEmulatorLib {
 		public void Resize(int column_width, int row_height) {
 			TheConsole?.Resize(column_width, row_height);
 		}
+		public void SetCursorVisibility(bool visible) => WriteToUITerminal("\x1b[?25" + (visible ? 'h' : 'l'));
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="fullReset">Means near all parameters of the term are reset to defaults rather than just clearing the screen</param>
+		public void ClearUITerminal(bool fullReset = false) => WriteToUITerminal(fullReset ? "\x001bc\x1b]104\x1b\\" : "\x1b[H\x1b[2J\u001b[3J");
+
 		void ITerminalConnection.Close() {
 			TheConsole?.Dispose();
 		}
